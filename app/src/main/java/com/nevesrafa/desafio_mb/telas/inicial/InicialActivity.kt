07fe62624a84480af5f1993e09a3e459ba56b615
Rafael.Neves <rafaelneves02@gmail.com>
2021-06-19 11:37:45 -0300

    Vinculado Nome e Foto do usuario utilizando Firebase e Glide. Modificação de cores Tema e botões. Criado Botão Sair.

--- app/src/main/java/com/nevesrafa/desafio_mb/telas/inicial/InicialActivity.kt
+++ app/src/main/java/com/nevesrafa/desafio_mb/telas/inicial/InicialActivity.kt
@@ -3,6 +3,9 @@ package com.nevesrafa.desafio_mb.telas.inicial
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.nevesrafa.desafio_mb.R
 import com.nevesrafa.desafio_mb.databinding.AcitivityInicialBinding
 import com.nevesrafa.desafio_mb.telas.eventos.EventosActivity
 import com.nevesrafa.desafio_mb.telas.meus_ingressos.MeusIngressosActivity
@@ -16,6 +19,13 @@ class InicialActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = AcitivityInicialBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val usuario = FirebaseAuth.getInstance().currentUser
+        binding.usuario.text = getString(R.string.ola, usuario?.displayName)
+        Glide.with(this)
+            .load(usuario?.photoUrl)
+            .circleCrop()
+            .into(binding.imagemUsuario)
 
         presenter = InicialPresenter(this)
 
